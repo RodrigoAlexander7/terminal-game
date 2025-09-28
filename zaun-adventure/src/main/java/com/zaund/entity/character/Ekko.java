647cbe0b15fed8d8c872ac324aca16ce7a4e40a3
RodryAlexander <rodrygoleu7@gmail.com>
2025-09-28 18:15:31 -0500

    Adding basic atack logic

--- zaun-adventure/src/main/java/com/zaund/entity/character/Ekko.java
+++ zaun-adventure/src/main/java/com/zaund/entity/character/Ekko.java
@@ -6,9 +6,15 @@ import com.zaund.entity.Entity;
 public class Ekko extends Player{
    public static final int POWER_LEVEL = 8500;  // the power capacity
    public static final int LIFE_POINTS = 8500;  // the life capacity
+   public static final int BASIC_ATACK_POWER = 500;
+   public static final int MIDDLE_ATACK_POWER = 1000;
+   public static final int MAX_ATACK_POWER = 2000;
+
 
    public void basicAtack(Enemy enemy){
-
+      System.out.println("Direct hit");
+      enemy.receiveAttack(BASIC_ATACK_POWER);
+      this.powerStatus -= BASIC_ATACK_POWER / 2;
    }
    public void middleAtack(Enemy enemy){
 
--- zaun-adventure/src/main/java/com/zaund/entity/character/Enemy.java
+++ zaun-adventure/src/main/java/com/zaund/entity/character/Enemy.java
@@ -1,7 +1,26 @@
 // abstract class to shepe all the players (heros)
 package com.zaund.entity.character;
 
-public abstract class Enemy {
+import com.zaund.entity.Entity;
 
+public abstract class Enemy extends Entity{
+   protected int life;
+   protected boolean isLive;
+
+   public Enemy(int life){
+      this.life = life; 
+      isLive = true;
+   }
+
+   public abstract void basicAtack(Player player);
+   
+   public void receiveAttack(int attackStat){
+      life = life - attackStat;
+      if(life <= 0){
+         isLive = false;
+         System.out.println("You finish the enemy!");
+      }
+
+   }
 
 }
