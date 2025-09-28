587fa0bd36f97f681357c7173f60819fbb8f760d
RodryAlexander <rodrygoleu7@gmail.com>
2025-09-27 21:21:02 -0500

    Implementing classes to generate a Map

---
+++ zaun-adventure/src/main/java/com/zaund/map/Map.java
@@ -0,0 +1,29 @@
+package com.zaund.map;
+
+public class Map{
+   private Tile[][] grid;
+   private int numR; //number of rows
+   private int numC; //number of columns
+
+   public Map(int numR, int numC){
+      this.grid = new Tile[numR][numC];
+      this.numR = numR;
+      this.numC = numC;
+   }
+
+   public int getNumR(){ return numR; }
+
+   public int getNumC(){ return numC; }
+
+   public boolean isPositionTransitable(int row, int col){
+      return grid[row][col].getTransitable();
+   }
+
+   public Tile getTile(int row, int col){
+      return grid[row][col];
+   }
+
+   public void setTile(int row, int col, Tile tile){
+      grid[row][col] = tile;
+   }
+}
---
+++ zaun-adventure/src/main/java/com/zaund/map/MapGenerator.java
@@ -0,0 +1,18 @@
+package com.zaund.map;
+
+public class MapGenerator{
+   public Map basicMap(){
+      Map basic = new Map(30,30);
+      int numR = basic.getNumR();
+      int numC = basic.getNumC();
+      for(int i = 0; i < numR; i++){
+         for(int j = 0 ; i < numC ; j++){
+            if(i == 0 || i == numR - 1 || j == 0 || j == numC -1){
+               basic.setTile(i, j, Tile.WALL);
+            }
+         }
+      }
+      return basic;
+   }
+
+}
