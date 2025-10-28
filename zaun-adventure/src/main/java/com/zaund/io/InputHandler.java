003ea7210adc141e972bcbcf5a47a647ca20cec5
RodryAlexander <rodrygoleu7@gmail.com>
2025-10-28 13:18:22 -0500

    feat: improving terminal render with JLine

--- zaun-adventure/src/main/java/com/zaund/io/InputHandler.java
+++ zaun-adventure/src/main/java/com/zaund/io/InputHandler.java
@@ -3,28 +3,31 @@ package com.zaund.io;
 import java.util.Scanner;
 
 import org.jline.terminal.Terminal;
-import org.jline.terminal.TerminalBuilder;
 
 public class InputHandler {
-    private static Terminal terminal;
-
-    static {
-        try {
-            terminal = TerminalBuilder.terminal();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     public static Command getExploringInput() {
         try {
+            Terminal terminal = ConsoleRenderer.getTerminal();
+            if (terminal == null) {
+                return Command.INVALID;
+            }
+            
             // the int is the code on ascii of the char
             int ch = terminal.reader().read(); // read without enter
             switch (ch) {
-                case 'w': return Command.MOVE_UP;
-                case 's': return Command.MOVE_DOWN;
-                case 'a': return Command.MOVE_LEFT;
-                case 'd': return Command.MOVE_RIGHT;
+                case 'w':
+                case 'W': return Command.MOVE_UP;
+                case 's':
+                case 'S': return Command.MOVE_DOWN;
+                case 'a':
+                case 'A': return Command.MOVE_LEFT;
+                case 'd':
+                case 'D': return Command.MOVE_RIGHT;
+                case 'q':
+                case 'Q': return Command.EXIT;
+                case 27:  // ESC key
+                    return Command.EXIT;
                 default:  return Command.INVALID;
             }
         } catch (Exception e) {
@@ -32,6 +35,7 @@ public class InputHandler {
             return Command.INVALID;
         }
     }
+    
     public static Command getMenuInput(){
         System.out.println(">");
         Scanner sc = new Scanner(System.in);
